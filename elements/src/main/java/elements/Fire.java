5f78aedb30eae957363669c1f33407f267d99196
yahoza <quentin.lebarillier@viacesi.fr>
2016-06-14 11:23:53 +0200

    Fire

---
+++ elements/src/main/java/elements/Fire.java
@@ -0,0 +1,43 @@
+package elements;
+
+public class Fire extends Element {
+	private int TYPE = 7;
+	private boolean PENETRABLE = true;
+	private int PosX;
+	private int PosY;
+	
+	public boolean getPENETRABLE() {
+		return PENETRABLE;
+	}
+
+	public void setPENETRABLE(boolean penetrable) {
+		PENETRABLE = penetrable;
+	}
+
+	public int getTYPE() {
+		return TYPE;
+	}
+
+	public void setTYPE(int type) {
+		TYPE = type;
+	}
+	
+	public int getPosX() {
+		return PosX;
+	}
+	
+	public void setPosX(int posX) {
+		PosX = posX;
+	}
+	
+	public int getPosY() {
+		return PosY;
+	}
+	
+	public void setPosY(int posY) {
+		PosY = posY;
+	}
+}
+
+
+}
