9363b3705c38106d28178b975fb467348d456bf3
ChocoP0ps <alexandre.cano@viacesi.fr>
2016-06-20 14:30:31 +0200

    javadoc wall

--- model/src/main/java/elements/Wall.java
+++ model/src/main/java/elements/Wall.java
@@ -1,6 +1,12 @@
 package elements;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Wall.
+ */
 public class Wall extends Element{
+	
+	/** The penetrable. */
 	@SuppressWarnings("unused")
 	private boolean PENETRABLE = false;
 }
